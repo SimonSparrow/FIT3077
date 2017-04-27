4daa486efad4261117dece6e54580e7264e6a6c1
KhoziKony <nnde3@student.monash.edu>
2017-04-27 07:18:03 +0200

    Update CfrmWeather.cs
    Update with working, still need to fix remove button

--- CfrmWeather.cs
+++ CfrmWeather.cs
@@ -11,95 +11,152 @@ using Hopeful.MelborneWeatherService;
 
 namespace Hopeful
 {
-    public partial class CfrmWeather : Form
-    {
-        GetData data = new GetData();
-        public CheckBox check;
-        public Label label;
-        public CfrmWeather()
-        {
-            InitializeComponent();
-        }
+	public partial class CfrmWeather : Form
+	{
+		GetData data = new GetData();
+		public CheckBox check;
+		public Label labelR;
+		public Label labelT;
+		public CfrmWeather()
+		{
+			InitializeComponent();
+		}
 
-        private void cmbLocations_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            MelbourneWeather2PortTypeClient client = new MelbourneWeather2PortTypeClient("MelbourneWeather2HttpSoap12Endpoint");
-            
-            string location = cmbLocations.SelectedItem.ToString();
-            GetLocation local = new GetLocation(client, location);
-            data.RegisterObserver(local);
-            
-        }
+		private void cmbLocations_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			MelbourneWeather2PortTypeClient client = new MelbourneWeather2PortTypeClient("MelbourneWeather2HttpSoap12Endpoint");
 
-        private void CfrmWeather_Load(object sender, EventArgs e)
-        {
-            MelbourneWeather2PortTypeClient client = new MelbourneWeather2PortTypeClient("MelbourneWeather2HttpSoap12Endpoint");
-            GetLocation locations = new GetLocation(client);
-            //tmrUpdate.Start();
-            tlpLocations.Visible = false;
-            foreach (string location in locations.Locations())
-            {
-                cmbLocations.Items.Add(location);
-            }
-        }
+			string location = cmbLocations.SelectedItem.ToString();
+			GetLocation local = new GetLocation(client, location);
+			data.RegisterObserver(local);
+			
+		}
 
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            MelbourneWeather2PortTypeClient client = new MelbourneWeather2PortTypeClient("MelbourneWeather2HttpSoap12Endpoint");
+		private void CfrmWeather_Load(object sender, EventArgs e)
+		{
+			MelbourneWeather2PortTypeClient client = new MelbourneWeather2PortTypeClient("MelbourneWeather2HttpSoap12Endpoint");
+			GetLocation locations = new GetLocation(client);
+			//tmrUpdate.Start();
+			tlpLocations.Visible = true;
+			foreach (string location in locations.Locations())
+			{
+				cmbLocations.Items.Add(location);
+			}
+		}
 
-            string location = cmbLocations.SelectedItem.ToString();
-            GetLocation local = new GetLocation(client, location);
-            data.RegisterObserver(local);
-            tableAdd();
-        }
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			MelbourneWeather2PortTypeClient client = new MelbourneWeather2PortTypeClient("MelbourneWeather2HttpSoap12Endpoint");
 
-        private void btnRemove_Click(object sender, EventArgs e)
-        {
-            tableRemove();
-        }
+			string location = cmbLocations.SelectedItem.ToString();
+			GetLocation local = new GetLocation(client, location);
+			data.RegisterObserver(local);
+			tableAdd();
+		}
 
-        private void tmrUpdate_Tick(object sender, EventArgs e)
-        {
-            data.NotifyObserver();
-        }
+		private void btnRemove_Click(object sender, EventArgs e)
+		{
+			tableRemove();
+		}
 
-        private void tableRemove()
-        {
-            if (check.Checked)
-            {
-                foreach (GetLocation location in data.observers)
-                {
-                    if (check.Text.Equals(location.location))
-                    {
-                        data.RemoveObserver(location);
-                        break;
-                    }
+		private void tmrUpdate_Tick(object sender, EventArgs e)
+		{
+			data.NotifyObserver();
+		}
 
-                }
-                tlpLocations.Controls.Remove(check);
-                tlpLocations.Controls.Remove(label);
-                tlpLocations.RowCount = data.observers.Count;              
-            }
-        }
+		private void tableRemove()
+		{
+			if (check.Checked)
+			{
+				foreach (GetLocation location in data.observers)
+				{
+					if (check.Text.Equals(location.location))
+					{
+						data.RemoveObserver(location);
+						break;
+					}
+				}
+				tlpLocations.Controls.Remove(check);
+				tlpLocations.Controls.Remove(labelR);
+				tlpLocations.Controls.Remove(labelT);
+				tlpLocations.RowCount = data.observers.Count;
+			}
+		}
 
-        private void tableAdd()
-        {
-            check = new CheckBox();
-            label = new Label();
-            check.AutoSize = true;
-            label.AutoSize = true;
-            tlpLocations.Visible = false;
-            foreach (GetLocation location in data.observers)
-            {
-                check.Text = location.location;
-                tlpLocations.Controls.Add(check);
-            }
-            foreach (GetLocation location in data.observers)
-            {
-                label.Text = location.getRainfall() + "\n" + location.getTemperature();
-                tlpLocations.Controls.Add(label);
-            }
-            tlpLocations.Visible = true;
-        }
-    }
+		private void tableAdd()
+		{
+			check = new CheckBox();
+			labelR = new Label();
+			labelT = new Label();
+			check.AutoSize = true;
+			labelR.AutoSize = true;
+			labelT.AutoSize = true;
+			tlpLocations.Visible = true;
+
+			foreach (GetLocation location in data.observers)
+			{
+				check.Text = location.location;
+				tlpLocations.Controls.Add(check);
+			}
+
+			//rainfall and temperature
+			if (ckbRainfall.Checked == true && ckbTemp.Checked == true)
+			{
+				foreach (GetLocation location in data.observers)
+				{
+					labelR.Text = "Rainfall:" + location.getRainfall();
+					labelT.Text = "Temperature:" + location.getTemperature();
+					tlpLocations.Controls.Add(labelR);
+					tlpLocations.Controls.Add(labelT);
+					labelR.Visible = true;
+				labelT.Visible = true;
+				}
+
+			}
+
+			//rainfall
+			if (ckbRainfall.Checked == true)
+			{
+				foreach (GetLocation location in data.observers)
+				{
+					labelR.Text = "Rainfall:" + location.getRainfall();
+					tlpLocations.Controls.Add(labelR);
+				}
+			}
+			else
+			{
+				labelR.Visible = true;
+			}
+
+			//temperature
+			if (ckbTemp.Checked == true)
+			{
+				foreach (GetLocation location in data.observers)
+				{
+					labelT.Text = "Temperature:" + location.getTemperature();
+					tlpLocations.Controls.Add(labelT);
+				}
+			}
+			else
+			{
+				labelT.Visible = true;
+			}
+		}
+
+		private void ckbRainfall_CheckedChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void ckbTemp_CheckedChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void tlpLocations_Paint(object sender, PaintEventArgs e)
+		{
+				
+		}
+	}
 }
+
